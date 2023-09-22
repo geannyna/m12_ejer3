4eddab72c8c9e03b6e3e8c9cce9bbcb69e6a454e
claudia <claudia.iacob@hotmail.com>
2023-09-22 19:59:32 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CreateCustomerComponent } from './create-customer/create-customer.component'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCustomerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// hay que quitar todo lo que hay en app.component.html
---
+++ src/app/create-customer/create-customer.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-create-customer',
+  templateUrl: './create-customer.component.html',
+  styleUrls: ['./create-customer.component.css']
+})
+export class CreateCustomerComponent implements OnInit{
+  customerForm :FormGroup = new FormGroup({});
+  customerInfo: any = null;
+
+  constructor() {}
+
+  ngOnInit(): void{
+    this.customerForm=new FormGroup ({
+      name  :new FormControl(''),
+      cif   :new FormControl(''),
+      city : new FormControl(''),
+      })
+  }
+  showCustomer() {
+    console.log(this.customerForm.value);
+    this.customerInfo = this.customerForm.value;
+  }
+}
+
+// hay que quitar todo lo que hay en app.component.html
+
